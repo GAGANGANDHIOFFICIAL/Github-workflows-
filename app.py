f8d0346d1f9dbbcc046e9c66c76926478e54b0f1
GaganGandhi <gagangandhi4080@gmail.com>
2026-06-28 12:26:47 +0530

    Adding new github action workflows

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("Gagan boi ")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World! Welcome to Flask 🚀"
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy",
+        "service": "Flask App"
+    }
+
+
+@app.route("/about")
+def about():
+    return "This is a basic Flask application."
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
